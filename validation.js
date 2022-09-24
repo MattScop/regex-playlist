8d6e0eff9044d339ae87143b1859f0a226a13322
MattScop <matteo.scopece@outlook.com>
2022-09-24 13:00:13 +0200

    Add validation function & event listener

--- validation.js
+++ validation.js
@@ -1,5 +1,25 @@
 // validation script here
 // Mode 1
-let regex = /[a-z]/gi;
+// let regex = /[a-z]/gi;
 // Mode 2
-let regexTwo = new RegExp(/[a-z]/, 'ig');
+// let regexTwo = new RegExp(/[a-z]/, 'ig');
+
+const regex = {
+    telephone: /^\d{11}$/
+};
+
+const inputs = document.getElementsByTagName('input');
+Array.from(inputs).forEach(input => {
+    input.addEventListener('keyup', () => {
+        validate(input, regex[input.name])
+    });
+});
+
+// validation function
+function validate(field, regex) {
+    if (regex.test(field.value)) {
+        field.className = 'valid'
+    } else {
+        field.className = 'invalid'
+    }
+};
