f6993c68a6002bc3435a1d5d56bfe46576957bea
MattScop <matteo.scopece@outlook.com>
2022-09-25 12:59:26 +0200

    Add regex for other values

--- validation.js
+++ validation.js
@@ -5,7 +5,11 @@
 // let regexTwo = new RegExp(/[a-z]/, 'ig');
 
 const regex = {
-    telephone: /^\d{11}$/
+    username: /^[a-z\d]{5,12}$/,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+    password: /^[\w@-]{8,20}$/,
+    telephone: /^\d{11}$/,
+    slug: /^[a-z\d-]{8,20}$/
 };
 
 const inputs = document.getElementsByTagName('input');
@@ -18,8 +22,8 @@ Array.from(inputs).forEach(input => {
 // validation function
 function validate(field, regex) {
     if (regex.test(field.value)) {
-        field.className = 'valid'
+        field.className = 'valid';
     } else {
-        field.className = 'invalid'
+        field.className = 'invalid';
     }
 };
